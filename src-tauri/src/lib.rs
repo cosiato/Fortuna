b49f451efa18eb7338c497c22a845034713984ee
tomtomdu73 <thomas.cosialls@gmail.com>
2026-02-16 01:10:17 -0500

    fix(updater): refactor update logic to handle errors and streamline installation process

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,7 +25,13 @@ pub fn run() {
 
             let handle = app.handle().clone();
             tauri::async_runtime::spawn(async move {
-                update(handle).await.unwrap();
+                let updater = match handle.updater() {
+                    Ok(u) => u,
+                    Err(_) => return,
+                };
+                if let Ok(Some(update)) = updater.check().await {
+                    let _ = update.download_and_install(|_, _| {}, || {}).await;
+                }
             });
 
             Ok(())
@@ -78,28 +84,4 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-  if let Some(update) = app.updater()?.check().await? {
-    let mut downloaded = 0;
-
-    // alternatively we could also call update.download() and update.install() separately
-    update
-      .download_and_install(
-        |chunk_length, content_length| {
-          downloaded += chunk_length;
-          println!("downloaded {downloaded} from {content_length:?}");
-        },
-        || {
-          println!("download finished");
-        },
-      )
-      .await?;
-
-    println!("update installed");
-    app.restart();
-  }
-
-  Ok(())
 }
